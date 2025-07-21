9defb6096ad7795d3b5b9f4264c7f249653a1f20
Aylin <aylgut2005@gmail.com>
2025-07-21 12:16:46 -0600

    Changes by JuanMa

--- src/PrincipalAdmin.java
+++ src/PrincipalAdmin.java
@@ -1,2 +1,16 @@
+import javax.swing.JPanel;
+
+
 public class PrincipalAdmin {
+    private JPanel panelPrincipalAdmin;
+
+    public PrincipalAdmin() {
+
+        panelPrincipalAdmin = new JPanel();
+
+    }
+
+    public JPanel getPanel() {
+        return panelPrincipalAdmin;
+    }
 }
